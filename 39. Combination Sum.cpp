91be41198b6c207eb74fc8a5a8e3ab4d1b545f02
VAIBHAV SINGHAL <v.singhal202@gmail.com>
2021-06-05 00:29:43 +0530

    Backtracking - medium
    Facebook | 13  Airbnb | 10  Microsoft | 7  Amazon | 4  Apple | 4  eBay | 3  Bloomberg | 3  Google | 2  Uber | 2  ByteDance | 2

---
+++ 39. Combination Sum.cpp
@@ -0,0 +1,28 @@
+#define ll long long 
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void backtrack(vector<int> &cand,  int target,vector<int> &temp, vector< vector<int>> &result, int begin)
+    {
+        if(target < 0 )
+            return;
+        if(target == 0)
+        {
+            result.push_back(temp);
+        }
+        for(int i= begin; i< cand.size(); i++ )
+        {
+            temp.push_back(cand[i] );
+            backtrack(cand, target - cand[i] ,temp,result,i );
+            temp.pop_back();
+        }
+    } 
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        vector<int> temp;
+        backtrack(candidates,target,temp, result,0);
+        return result;
+    }
+};
