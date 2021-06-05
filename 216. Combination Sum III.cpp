8ce8e10f3810fbc5b9ad605d9e9fc72a088c374e
VAIBHAV SINGHAL <v.singhal202@gmail.com>
2021-06-05 13:27:44 +0530

    medium - backtracking

---
+++ 216. Combination Sum III.cpp
@@ -0,0 +1,18 @@
+void backtrack(vector<int> &cand,  int target,vector<int> &temp, vector<vector<int>> &result, int begin)
+    {
+        if(target < 0 )
+            return;
+        if(target == 0)
+        {
+            result.push_back(temp);
+        }
+        for(int i=begin; i< cand.size(); i++ )
+        {
+            temp.push_back(cand[i] );
+            backtrack(cand, target - cand[i] ,temp,result,i+1 );
+            temp.pop_back();
+            // we don't need to put the same element at current positition
+            while( i< cand.size()-1 && cand[i] == cand[i+1])
+                i++;
+        }
+     }
