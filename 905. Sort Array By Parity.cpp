83247de7850f870ef6f4b07c4328d1440ceb4ee6
VAIBHAV SINGHAL <v.singhal202@gmail.com>
2021-07-13 17:04:36 +0530

    Create 905. Sort Array By Parity.cpp

---
+++ 905. Sort Array By Parity.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int i =0 , j=nums.size()-1;
+        while( i <= j )
+        {
+            while( nums[i] %2 == 0 && i<j )
+                i++;
+            while( nums[j] %2 != 0 && i<j )
+                j--;
+            swap(nums[i++] , nums[j--]);
+        }
+        return nums;
+    }
+};
